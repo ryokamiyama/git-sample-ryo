fc7dcbec8e0aa4c3f3d5adf08e6719fbdde3d8f8
kamiyama <ryo.kamiyama@rakus.co.jp>
2017-08-08 12:29:06 +0900

    練習用に作成

---
+++ src/main/java/jp/co/rakus/git_sample_kamiyama/Animal.java
@@ -0,0 +1,15 @@
+package jp.co.rakus.git_sample_kamiyama;
+
+public class Animal {
+
+	private String cry;
+
+	public String getCry() {
+		return cry;
+	}
+
+	public void setCry(String cry) {
+		this.cry = cry;
+	}
+
+}
