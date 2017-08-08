818d4cd4dbd2e7e63a4013d72e03f8da64a10838
kamiyama <ryo.kamiyama@rakus.co.jp>
2017-08-08 11:48:15 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/jp/co/rakus/git_sample_kamiyama/App.java
@@ -0,0 +1,13 @@
+package jp.co.rakus.git_sample_kamiyama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
