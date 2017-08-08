746b9bbecb35ccbf2a60154c431227ccf76ea6dd
kamiyama <ryo.kamiyama@rakus.co.jp>
2017-08-08 15:27:57 +0900

    CarクラスとAnimalクラスのフィールドに追加

--- src/main/java/jp/co/rakus/git_sample_kamiyama/Animal.java
+++ src/main/java/jp/co/rakus/git_sample_kamiyama/Animal.java
@@ -3,6 +3,15 @@ package jp.co.rakus.git_sample_kamiyama;
 public class Animal {
 
 	private String cry;
+	private int walk;
+
+	public int getWalk() {
+		return walk;
+	}
+
+	public void setWalk(int walk) {
+		this.walk = walk;
+	}
 
 	public String getCry() {
 		return cry;
--- src/main/java/jp/co/rakus/git_sample_kamiyama/Car.java
+++ src/main/java/jp/co/rakus/git_sample_kamiyama/Car.java
@@ -3,6 +3,16 @@ package jp.co.rakus.git_sample_kamiyama;
 public class Car {
 
 	private int speed;
+	private String energy;
+	
+	
+	public String getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(String energy) {
+		this.energy = energy;
+	}
 
 	public void run() {
 		this.speed += 5;
